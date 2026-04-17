caa235d45bcfc66bbe91d9ab1715c57846e62dd1
mtaipan <mbvlog07@gmail.com>
2026-04-17 19:06:00 +0300

    Initial Java version

---
+++ src/main/java/dev/taipan/TaipanLedger/TaipanLedgerApplication.java
@@ -0,0 +1,13 @@
+package dev.taipan.TaipanLedger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaipanLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaipanLedgerApplication.class, args);
+	}
+
+}
